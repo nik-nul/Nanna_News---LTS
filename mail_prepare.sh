8bb4f66a1fe637907577007a3a1cd7de2440bc61
nik-nul <nik_nul@protonmail.com>
2024-09-28 19:17:30 +0800

    Add Feed For Mail

--- mail_prepare.sh
+++ mail_prepare.sh
@@ -1,4 +1,4 @@
 echo '/^\\begin{document}/ {print; found=1; next} found && /^% HEAD END/ {print "\\title{南哪消息' $(TZ=Asia/Urumqi date "+%F") '}\\maketitle"; print; found=0; next} found {next} {gsub(/section/, "subsection"); gsub(/\\centerline{\\huge/, "\\section{"); gsub(/\\begin{multicols}\{2\}/, ""); gsub(/\\end{multicols}/, ""); print; }' > pat
 awk -f pat ./tex/000.tex > tmp.tex
 pandoc tmp.tex -s -o ./latest.md
-pandoc --toc ./latest.md -s -V colorlinks=true -V linkcolor=blue -V urlcolor=red -V toccolor=gray --css pandoc.css --embed-resources -o ./tmp.html
+pandoc --toc ./latest.md -s -V colorlinks=true -V linkcolor=blue -V urlcolor=red -V toccolor=gray --css pandoc.css -h mailhead.html --embed-resources -o ./tmp.html
